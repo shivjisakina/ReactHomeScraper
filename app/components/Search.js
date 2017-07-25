7c1b22cf8ba0b17911265f001130fc43927f8128
Sakina Shivji <shivjisakina@gmail.com>
2017-07-25 09:15:31 -0400

    added routes and npm packages for express

---
+++ app/components/Search.js
@@ -0,0 +1,16 @@
+// Include React
+var React = require("react");
+
+var Search = React.createClass({
+
+    // Here we render the component
+    render: function() {
+
+        return (
+
+        );
+    }
+});
+
+// Export the component back for use in other files
+module.exports = Search;
